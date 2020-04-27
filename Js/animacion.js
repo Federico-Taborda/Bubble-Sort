21d22049ed5d07f882a1682c47f83101e16f1d29
panterx <panterx.95@gmail.com>
2020-04-27 16:55:47 -0300

    Refactorizacion de obtener x

--- Js/animacion.js
+++ Js/animacion.js
@@ -7,19 +7,21 @@ class Animacion {
         // Color de elementos Rojo
         elemento1.cambiarEstilos("#ff4d4d");
         elemento2.cambiarEstilos("#ff4d4d");
+
         console.log(elemento1.valor, elemento1.x)
         console.log(elemento2.valor, elemento2.x)
+
         // Anima los dos elementos
         this.animacion(elemento1, elemento2.x - elemento1.x);
         await this.animacion(elemento2, elemento1.x - elemento2.x);
         
         // Obtiene la x de los elementos
-        elemento1.obtenerX();
-        await elemento2.obtenerX();
+        await Promise.all([elemento1.obtenerX(this.tiempo), elemento2.obtenerX(this.tiempo)]);
         
         console.log(elemento1.valor, elemento1.x)
         console.log(elemento2.valor, elemento2.x)
         console.log("===============")
+
         // Color de elementos Verde
         elemento1.cambiarEstilos("#2ecc71");
         elemento2.cambiarEstilos("#2ecc71");
--- Js/elementos.js
+++ Js/elementos.js
@@ -4,15 +4,14 @@ class Elemento {
         this.valor = parseInt(this.propiedad.children[0].textContent);
         this.style = window.getComputedStyle(this.propiedad);
         this.x = this.propiedad.getBoundingClientRect().x;
-        this.segundos = 400;
     };
 
     // Obtiene la x del elemento
-    obtenerX() {
+    obtenerX(time) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(this.x = this.propiedad.getBoundingClientRect().x);
-            }, this.segundos + 100);
+            }, time + 100);
         });
     };
     
